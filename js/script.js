93b87881247ba2daa81f79d5909eadc5c1bbf641
Alessandro <alessandrogattuso@yahoo.it>
2022-12-02 13:58:47 +0100

    exersise done

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const list = [
+  "Pomodoro",
+  "Pasta",
+  "Sale",
+  "Acqua",
+  "Peperoni",
+  "Fesa di tacchino"
+]
+
+let item;
+let i = 0;
+const shoppingList = document.querySelector("ul");
+
+while(i < list.length){
+  item = document.createElement("li");
+  item.innerText = list[i++];
+  shoppingList.append(item);
+}
