5678ab29af65c592e5744a445b0cbc4104d754fa
Alessandro <alessandrogattuso@yahoo.it>
2022-12-02 15:13:03 +0100

    added some classes

--- js/script.js
+++ js/script.js
@@ -14,5 +14,6 @@ const shoppingList = document.querySelector("ul");
 while(i < list.length){
   item = document.createElement("li");
   item.innerText = list[i++];
+  item.classList.add("list-group-item")
   shoppingList.append(item);
 }
